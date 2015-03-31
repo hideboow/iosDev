25e2afeddd87c736a1bdaa009cfe165caf629798
hideboow <hide_69ch@yahoo.co.jp>
2015-03-31 19:53:59 +0900

    天気アイコンまでの取得

--- weatherForecast/ViewController.m
+++ weatherForecast/ViewController.m
@@ -22,6 +22,7 @@
     NSMutableArray *dayArray;
     NSMutableArray *maxTemp;
     NSMutableArray *minTemp;
+    NSMutableArray *imageArray;
 }
 
 - (void)viewDidLoad {
@@ -46,30 +47,48 @@
     NSError *error = nil;
     NSDictionary *jsonObj = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableLeaves error:&error];
     weatherArray = [jsonObj valueForKeyPath:@"list"];
-    
-    //（週間予報用）曜日を取得
-    dayArray = [[NSMutableArray alloc]init];
-    for (int i=0; i<7; i++) {
-        NSTimeInterval unixTime = [[jsonObj valueForKeyPath:@"list.dt"][i] doubleValue];
-        NSDate *correctData = [NSDate dateWithTimeIntervalSince1970:unixTime];
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        [dateFormatter setDateFormat:@"EEE"];
-        NSString *dateStr = [dateFormatter stringFromDate:correctData];
-        [dayArray addObject:dateStr];
+
+    if (weatherArray != nil) {
+        
+        //（週間予報用）曜日を取得
+        dayArray = [[NSMutableArray alloc]init];
+        for (int i=0; i<7; i++) {
+            NSTimeInterval unixTime = [[jsonObj valueForKeyPath:@"list.dt"][i] doubleValue];
+            NSDate *correctData = [NSDate dateWithTimeIntervalSince1970:unixTime];
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            [dateFormatter setDateFormat:@"EEE"];
+            NSString *dateStr = [dateFormatter stringFromDate:correctData];
+            [dayArray addObject:dateStr];
+        }
+        
+        //（週間予報用）気温を取得
+        maxTemp = [[NSMutableArray alloc]init];
+        minTemp = [[NSMutableArray alloc]init];
+        for (int i=0; i<7; i++) {
+            NSNumber *maxTempNum = [jsonObj valueForKeyPath:@"list.temp.max"][i];
+            NSString *maxTempStr = [NSString stringWithFormat:@"%ld",(long)[maxTempNum integerValue]];
+            NSNumber *minTempNum = [jsonObj valueForKeyPath:@"list.temp.min"][i];
+            NSString *minTempStr = [NSString stringWithFormat:@"%ld",(long)[minTempNum integerValue]];
+            [maxTemp addObject:maxTempStr];//最高気温
+            [minTemp addObject:minTempStr];//最低気温
+        }
+        
+        //今日の気温を表示
+        self.tempLabel.text = [NSString stringWithFormat:@"%@℃",maxTemp[0]];
+        
+        //（週間予報用）天気アイコンを取得
+        imageArray = [[NSMutableArray alloc]init];
+        for (int i=0; i<7; i++) {
+            NSString *iconNo = [jsonObj valueForKeyPath:@"list.weather.icon"];
+            NSString *iconStr = [NSString stringWithFormat:@"http://openweathermap.org/img/w/%@.png",iconNo];
+            NSURL *iconUrl = [NSURL URLWithString:iconStr];
+            NSData *iconData = [NSData dataWithContentsOfURL:iconUrl];
+            UIImage *iconImage = [UIImage imageWithData:iconData];
+            [imageArray addObject:iconImage];
+        }
+    }else{
+        NSLog(@"get json failed");
     }
-    
-    //（週間予報用）気温を取得
-    maxTemp = [[NSMutableArray alloc]init];
-    minTemp = [[NSMutableArray alloc]init];
-    for (int i=0; i<7; i++) {
-        NSNumber *maxTempNum = [jsonObj valueForKeyPath:@"list.temp.max"][i];
-        NSString *maxTempStr = [NSString stringWithFormat:@"%ld",(long)[maxTempNum integerValue]];
-        NSNumber *minTempNum = [jsonObj valueForKeyPath:@"list.temp.min"][i];
-        NSString *minTempStr = [NSString stringWithFormat:@"%ld",(long)[minTempNum integerValue]];
-        [maxTemp addObject:maxTempStr];//最高気温
-        [minTemp addObject:minTempStr];//最低気温
-    }
-    
 }
 
 //表示する行数を設定
@@ -82,9 +101,11 @@
     static NSString *CellIdentifier = @"Cell";
     CustomTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     
-    cell.dayLabel.text = [NSString stringWithFormat:@"%@day",[dayArray objectAtIndex:indexPath.row]];//曜日を表示
-    cell.maxTempLabel.text = [NSString stringWithFormat:@"%@℃",[maxTemp objectAtIndex:indexPath.row]];//最高気温を表示
-    cell.minTempLabel.text = [NSString stringWithFormat:@"%@℃",[minTemp objectAtIndex:indexPath.row]];//最低気温を表示
+    //取得した情報を表示
+    cell.dayLabel.text = [NSString stringWithFormat:@"%@day",[dayArray objectAtIndex:indexPath.row]];
+    cell.maxTempLabel.text = [NSString stringWithFormat:@"%@℃",[maxTemp objectAtIndex:indexPath.row]];
+    cell.minTempLabel.text = [NSString stringWithFormat:@"%@℃",[minTemp objectAtIndex:indexPath.row]];
+    cell.imageView.image = [imageArray objectAtIndex:indexPath.row];
     return cell;
 }
 
